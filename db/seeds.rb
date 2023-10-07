94615048715b440aa4a110e9f2e2e0d7c4f287cf
otavio gomes <otaviojdggomes@gmail.com>
2023-10-07 16:55:42 -0300

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Article.destroy_all
+
+# Create 10 fake articles
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    content: Faker::Lorem.paragraph(sentence_count: 10)
+  )
+end
